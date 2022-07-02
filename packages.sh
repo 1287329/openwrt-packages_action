b120bf98b059db1b248cd0f123165ce66321cd96
1287329 <61381370+1287329@users.noreply.github.com>
2022-07-02 16:46:20 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #git clone https://github.com/1287329/luci-app-adbyby-plus ./luci-app-adbyby-plus
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
-git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-filebrowser
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-aliddns
 svn export https://github.com/immortalwrt/packages/trunk/net/smartdns
 svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome
 svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
 svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2

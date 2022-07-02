31c74b66f7ebba0e37456c4280957433214038e9
1287329 <61381370+1287329@users.noreply.github.com>
2022-07-02 16:31:04 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-#!/bin/bash
 #git clone https://github.com/1287329/luci-app-adbyby-plus ./luci-app-adbyby-plus
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ttyd
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd
-git clone --depth 1 https://github.com/kiddin9/luci-app-dnsfilter
-git clone --depth 1 https://github.com/kiddin9/aria2
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant
 svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-filebrowser
 svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-aliddns
@@ -16,7 +10,8 @@ svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome
 svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
 svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
-
+svn export https://github.com/1287329/luci-app-adbyby-plus
+svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby
 rm -rf .svn
 rm -rf ./*/.git
 rm -rf ./*/.svn 

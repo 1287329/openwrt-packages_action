6faaba1e8b278521f6286f3b007530ff6b89089f
1287329 <61381370+1287329@users.noreply.github.com>
2022-07-03 12:09:12 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-#git clone https://github.com/1287329/luci-app-adbyby-plus ./luci-app-adbyby-plus
-svn export https://github.com/immortalwrt/packages/trunk/net/smartdns
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
 git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome
+svn export https://github.com/immortalwrt/packages/trunk/net/smartdns
+svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
 svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
-svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
-svn export https://github.com/1287329/luci-app-adbyby-plus ./luci-app-adbyby-plus
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome
+svn export https://github.com/kiddin9/openwrt-packages/trunk/adbyby
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adbyby-plus
+svn export https://github.com/kiddin9/openwrt-packages/trunk/filebrowser
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-filebrowser
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-rclone
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-store
+svn export https://github.com/kiddin9/openwrt-packages/trunk/netdata
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netdata
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netspeedtest
+#svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby
+#svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-filebrowser
+#svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-aliddns
+
+
 rm -rf .svn
 rm -rf ./*/.git
 rm -rf ./*/.svn 

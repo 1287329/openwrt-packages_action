ab60896927ea397a394024028db2697ff860d0cc
1287329 <61381370+1287329@users.noreply.github.com>
2022-09-04 23:32:23 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,7 +2,6 @@
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
 #git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome
-#svn export https://github.com/rufengsuixing/luci-app-adguardhome
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/adbyby
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adbyby-plus
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/filebrowser
@@ -11,6 +10,9 @@
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netdata
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netspeedtest
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd
+svn export https://github.com/kiddin9/openwrt-packages/trunk/filebrowser
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-filebrowser
+#svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-filebrowser
 #svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby
 #svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
 #git clone https://github.com/PikuZheng/smartdns

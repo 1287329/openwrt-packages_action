8ad43f5a9668f630d6153415407a432ef4a4f0ad
1287329 <61381370+1287329@users.noreply.github.com>
2022-09-04 22:54:06 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,7 +6,7 @@
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/adbyby
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adbyby-plus
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/filebrowser
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-filebrowser
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-filebrowser
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-rclone
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/netdata
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netdata
@@ -17,12 +17,14 @@
 #svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-aliddns
 #svn export https://github.com/immortalwrt/packages/trunk/net/smartdns
 #svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
+#git clone https://github.com/PikuZheng/smartdns
 svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
-git clone https://github.com/xiaorouji/openwrt-passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
 svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
 git clone https://github.com/sbwml/luci-app-alist
-git clone https://github.com/PikuZheng/smartdns
+
 
 
 rm -rf .svn
